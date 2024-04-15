a44fc07b01158c6df3d9fe4962277bb7001b8d9d
Rune2292 <132446427+Rune2292@users.noreply.github.com>
2024-04-15 12:38:11 +0200

    fix: tried to fix it

--- cqrs/Program.cs
+++ cqrs/Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,8 +14,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
+app.UseHttpsRedirection();
 
 var summaries = new[]
 {
